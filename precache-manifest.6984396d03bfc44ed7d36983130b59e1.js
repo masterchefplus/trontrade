e307834147bf40244101fe29b56bc71a64a80cfb
masterchefplus <72683409+masterchefplus@users.noreply.github.com>
2020-10-18 09:44:31 +0700

    Add files via upload

---
+++ precache-manifest.6984396d03bfc44ed7d36983130b59e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edac3bbc01a610a2e0771cae5cc12b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabe08a7e2296d59b74f",
+    "url": "/static/css/main.aafccee4.chunk.css"
+  },
+  {
+    "revision": "6ce288524bfd679bbc8b",
+    "url": "/static/js/2.791cf922.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/static/js/2.791cf922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eabe08a7e2296d59b74f",
+    "url": "/static/js/main.f04ea5ec.chunk.js"
+  },
+  {
+    "revision": "0731241a274324d49d43",
+    "url": "/static/js/runtime-main.74b0f3b2.js"
+  },
+  {
+    "revision": "170f0ec0b00a277c79954edaa14a37e5",
+    "url": "/static/media/1553966479995.170f0ec0.jpg"
+  },
+  {
+    "revision": "a3a5959f48a1bfe358fcb63477b09898",
+    "url": "/static/media/1553966624078.a3a5959f.jpg"
+  },
+  {
+    "revision": "c9560b70b2531c37b0eb20218ffb4d0f",
+    "url": "/static/media/1553966631928.c9560b70.jpg"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "/static/media/TronLinkLogo.d3a8f115.png"
+  },
+  {
+    "revision": "271690b6a365d4d573bb6741bc3f7630",
+    "url": "/static/media/discordLogo.271690b6.png"
+  },
+  {
+    "revision": "91747f02b8603065a13e602820f9a189",
+    "url": "/static/media/telegramLogo.91747f02.png"
+  }
+]);
